94fdaccfbbe2d8852f54707decfd67b98f3e9be8
Andrej Jankovikj <152323152+AndrejJankovikj@users.noreply.github.com>
2023-12-08 13:53:58 +0100

    push so vtoroto domasno od posledniot cas

---
+++ BasicJavascriptHomework/Homework4.1 dogYears/script.js
@@ -0,0 +1,40 @@
+console.log("Dog Years");
+
+function calculateDogYears(dogYears) {
+    let result = dogYears * 7;
+    return result;
+
+
+}
+let result1 = calculateDogYears(5);
+let result2 = calculateDogYears(15);
+let result3 = calculateDogYears(8);
+console.log(result1);
+console.log(result2);
+console.log(result3);
+
+
+
+function calculateHumanYears(humanYears) {
+    rezultat = humanYears / 7;
+    return rezultat;
+
+}
+let rezultat1 = calculateHumanYears(35);
+let rezultat2 = calculateHumanYears(140);
+let rezultat3 = calculateHumanYears(63);
+console.log(rezultat1);
+console.log(rezultat2);
+console.log(rezultat3);
+
+
+alert('lean');
+
+
+
+
+
+
+
+
+
